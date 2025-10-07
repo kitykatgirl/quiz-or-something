4086c550e0e4be98e4488ae62500c4cf6594b823
kitykatgirl <kamilord220@vp.pl>
2025-10-07 08:12:14 +0200

    first commit about stuffs up to pytanie class

---
+++ app/src/main/java/ph/me/myapplication/Pytanie.java
@@ -0,0 +1,40 @@
+package ph.me.myapplication;
+
+public class Pytanie {
+    private int idObrazek;
+    private String trescPytania;
+    private boolean odpowiedz; // true - tak   false - nie
+    private String podpowiedzi;
+    private boolean czyOdpOK = false;
+
+    public Pytanie(String trescPytania, int idObrazek, boolean odpowiedz, String podpowiedzi) {
+        this.trescPytania = trescPytania;
+        this.idObrazek = idObrazek;
+        this.odpowiedz = odpowiedz;
+        this.podpowiedzi = podpowiedzi;
+    }
+
+    public int getIdObrazek() {
+        return idObrazek;
+    }
+
+    public String getTrescPytania() {
+        return trescPytania;
+    }
+
+    public boolean isOdpowiedz() {
+        return odpowiedz;
+    }
+
+    public String getPodpowiedzi() {
+        return podpowiedzi;
+    }
+
+    public boolean isCzyOdpOK() {
+        return czyOdpOK;
+    }
+
+    public void setCzyOdpOK(boolean czyOdpOK) {
+        this.czyOdpOK = czyOdpOK;
+    }
+}
